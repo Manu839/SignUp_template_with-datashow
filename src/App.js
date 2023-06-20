d91ffb355746d56b4a2f5528a1770d9caf2f5f8e
Manu839 <124304448+Manu839@users.noreply.github.com>
2023-06-21 01:09:32 +0530

    Signup template with validations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [userData, setuserData] = useState({
+		fullname:"",
+		username:"",
+		email:"",
+		password:"",
+
+	})
+	const [record, setrecord] = useState([])
+
+	const handleData =(e)=>{
+		const name = e.target.name;
+		const value = e.target.value;
+		console.log(name,value)
+		setuserData({...userData , [name]:value})
+	}
+	const handleSubmit =(e)=>{
+		e.preventDefault();
+		const newrecord = {...userData , id : new Date().getTime().toString()}
+		console.log(record)
+		setrecord([...record,newrecord]);
+		console.log(record)
+		setuserData({username:"",fullname:"",email:"",password:""})
+	}
+	return (
+		<>
+			<div className="h-screen overflow-hidden flex items-center justify-center bg-white" >
+				<div className="h-screen w-screen md:flex">
+					<div
+						className="relative overflow-hidden md:flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 rounded-r-2xl justify-around items-center hidden">
+						<div className='text-white'>
+							<h1 className="text-white font-bold text-5xl font-sans">Welcome to Sign Up</h1>
+							<p className="text-white text-lg mt-1">Have an account ??</p>
+							<button type="submit" className="block w-28 bg-white text-indigo-800 hover:bg-gradient-to-bl from-purple-600 to-pink-300 mt-6 py-2 rounded-2xl font-bold mb-2">Login</button>
+					
+
+							{
+								record.map((data)=>{
+									const{ id, fullname , username  , email ,password} = data;
+
+									return(
+									<span key={id}>
+										<p>Your filled details :</p>
+										<p>Full name: {fullname}
+										</p>
+										<p> Username : {username}</p>
+										<p>Email : {email}</p>
+										<p>Password : {password}</p>
+									</span>
+									)
+								})
+							}
+					</div>
+						<div className="absolute animate-pulse -bottom-32 -left-40 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+						<div className="absolute animate-pulse -bottom-40 -left-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+						<div className="absolute animate-pulse -top-40 -right-0 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+						<div className="absolute animate-pulse -top-20 -right-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+					</div>
+					<div className="flex md:w-1/2 justify-center py-10 items-center bg-white">
+						<form className="bg-white" onSubmit={handleSubmit}>
+							<h1 className="text-gray-800 font-bold text-3xl text-center mb-1 p-6">Welcome !!</h1>
+							<div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+								<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
+									fill="currentColor">
+									<path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+										clipRule="evenodd" />
+								</svg>
+								<input name ="fullname" value={userData.fullname} onChange={handleData} className="pl-2 outline-none border-none" type="text" placeholder="Full name" />
+							</div>
+							<div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+								<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none"
+									viewBox="0 0 24 24" stroke="currentColor">
+									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+										d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4" />
+								</svg>
+								<input name ="username" value={userData.username} onChange={handleData} className="pl-2 outline-none border-none" type="text" placeholder="Username" />
+							</div>
+							<div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+								<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none"
+									viewBox="0 0 24 24" stroke="currentColor">
+									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+										d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+								</svg>
+								<input name ="email" value={userData.email} onChange={handleData} className="pl-2 outline-none border-none" type="Email" placeholder="Email Address"/>
+							</div>
+							<div className="flex items-center border-2 py-2 px-3 rounded-2xl">
+								<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
+									fill="currentColor">
+									<path fillRule="evenodd"
+										d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+										clipRule="evenodd" />
+								</svg>
+								<input name ="password" value={userData.password} onChange={handleData} className="pl-2 outline-none border-none" type="Password" placeholder="Password" />
+							</div>
+							<button type="submit" className="block w-full bg-indigo-600 mt-8 py-2
+               rounded-2xl text-white font-semibold mb-2">Sign Up</button>
+							<span className="text-sm ml-2 hover:text-blue-500 cursor-pointer">Forgot Password ?</span>
+						</form>
+					</div>
+				</div>
+			</div>
+
+		</>
+	);
 }
 
 export default App;
